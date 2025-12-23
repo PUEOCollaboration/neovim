f9180ffbb30aa9004a8bc6ba04d089576602a9f3
jason yao <yao.966@osu.edu>
2025-12-23 08:26:29 +0000

    lsp updated for nvim 0.11+

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,22 +5,54 @@ return {
     dependencies = {
       "hrsh7th/cmp-nvim-lsp",
     },
-    config = function()
-      local lspconfig = require('lspconfig')
-      lspconfig.clangd.setup{}
-      lspconfig.neocmake.setup{}
-      lspconfig.pylsp.setup{
-        settings = {
-          pylsp = {
+    vim.lsp.config('pylsp', {
+      settings = {
+        ['pylsp'] = {
             plugins = {
               pycodestyle = {
                 ignore = {'E266','W503', 'W504', 'W605'},
                 maxLineLength = 110
               }
             }
-          }
-        }
-      }
-    end,
+        },
+      },
+    }),
+    vim.lsp.enable('pylsp'),
+    vim.lsp.enable('clangd'),
+    vim.lsp.config('neocmakelsp', {
+      init_options = {
+        ['lint'] = {
+          enable = false
+        },
+      },
+    }),
+    vim.lsp.enable('neocmake'),
+
+
+    -- old syntax, deprecated after neovim 0.11
+
+    -- config = function()
+    --   local lspconfig = require('lspconfig')
+    --   lspconfig.clangd.setup{}
+    --   lspconfig.neocmake.setup{
+    --     init_options = {
+    --       lint = {
+    --         enable = false
+    --       }
+    --     }
+    --   }
+    --   lspconfig.pylsp.setup{
+    --     settings = {
+    --       pylsp = {
+    --         plugins = {
+    --           pycodestyle = {
+    --             ignore = {'E266','W503', 'W504', 'W605'},
+    --             maxLineLength = 110
+    --           }
+    --         }
+    --       }
+    --     }
+    --   }
+    -- end,
   },
 }
